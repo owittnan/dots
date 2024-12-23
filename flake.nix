20f8989d3814efcf66232c814db564392dfed501
Oz Wittams-Nangle <owittnan@gmail.com>
2024-12-23 15:33:46 +0000

    fix syntax error and add home.nix
    whoops

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.owittnan = import ./hosts/galileo/home.nix;
-        };
+        }
       ];
     };
 
---
+++ hosts/galileo/home.nix
@@ -0,0 +1,61 @@
+{config, pkgs, ...}: {
+    home.username = "owittnan";
+    home.homeDirectory = "/home/owittnan";
+    home.stateVersion = "24.11"; 
+
+    home.packages = with pkgs; [
+        awscli2
+        direnv
+        ffmpeg
+        jj
+        lazygit
+        nmap
+        typescript
+        python3
+        ripgrep
+        watch
+        yt-dlp
+    ];
+
+    programs.home-manager.enable = true;
+
+    programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+    };
+
+    programs.zsh = {
+        enable = true;
+        shellAliases = {
+            rebuild = "darwin-rebuild switch --flake $HOME/.config/nix";
+        };
+        initExtra = ''
+            [[ ! -f ${./dots/zsh/p10k.zsh} ]] || source ${./dots/zsh/p10k.zsh}
+        '';
+        zplug = {
+            enable = true;
+            plugins = [
+                { name = "zsh-users/zsh-autosuggestions"; }
+                { name = "zsh-users/zsh-syntax-highlighting"; }
+                { name = "romkatv/powerlevel10k"; tags = [ as:theme ]; }
+            ];
+        };
+    };
+
+    programs.fish = {
+        enable = true;
+        shellAliases = {
+            rebuild = "sudo nixos-rebuild switch --flake $HOME/.config/nixos#";
+            lg = "lazygit";
+        };
+    };
+
+#     home.file = {
+#         ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/hosts/copernicus/dots/nvim";
+#         ".config/neovide/config.toml".text = ''
+#             neovim-bin = "/etc/profiles/per-user/owittnan/bin/nvim"
+#         '';
+#     };
+}
