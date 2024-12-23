f95250157d48de275018a2b88c0fe572776e560f
Oz Wittams-Nangle <owittnan@gmail.com>
2024-12-23 16:08:52 +0000

    use nixos-unstable instead of nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   description = "My Darwin system flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.nix-darwin = {
-    url = "github:LnL7/nix-darwin";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  inputs.home-manager = {
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
