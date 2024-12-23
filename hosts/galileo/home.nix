9e1aba58c20013bc03bdc77e42f5e5837487f61c
Oz Wittams-Nangle <owittnan@gmail.com>
2024-12-23 15:49:49 +0000

    remove zsh config

--- hosts/galileo/home.nix
+++ hosts/galileo/home.nix
@@ -26,24 +26,6 @@
         vimAlias = true;
     };
 
-    programs.zsh = {
-        enable = true;
-        shellAliases = {
-            rebuild = "darwin-rebuild switch --flake $HOME/.config/nix";
-        };
-        initExtra = ''
-            [[ ! -f ${./dots/zsh/p10k.zsh} ]] || source ${./dots/zsh/p10k.zsh}
-        '';
-        zplug = {
-            enable = true;
-            plugins = [
-                { name = "zsh-users/zsh-autosuggestions"; }
-                { name = "zsh-users/zsh-syntax-highlighting"; }
-                { name = "romkatv/powerlevel10k"; tags = [ as:theme ]; }
-            ];
-        };
-    };
-
     programs.fish = {
         enable = true;
         shellAliases = {
