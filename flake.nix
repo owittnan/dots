82e73eb01ae319425c2ecccc198885c09be5814c
Oz Wittams-Nangle <owittnan@gmail.com>
2024-12-24 13:34:52 +0000

    add home-manager backup config

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "hm-bak";
           home-manager.users.owittnan = import ./hosts/galileo/home.nix;
         }
       ];
